dd5c5a0523c5ca961499be3f503f1e0aadf03e41
sarahchang2013 <sarah_sarah99@hotmail.com>
2017-12-21 19:33:03 +0800

    bind data in html tags

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 var ViewModel = function() {
-	//define model (functions are still seperated)
+	//Define models (functions are still seperated)
+	//Bind with <div data-bind="text: clickCount"></div>
 	this.clickCount = ko.observable(0);
+	//Bind with h2 in html via data-bind="text: name"
 	this.name = ko.observable('Tony');
+	//Bind with attr: {src: imgSrc} to set img src
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('https://www.flickr.com');
 

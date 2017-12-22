0249b9302f9115bfd59b8f499ef0cdd923db5e2b
sarahchang2013 <sarah_sarah99@hotmail.com>
2017-12-22 16:44:19 +0800

    pass clicked model to click listener

--- js/app.js
+++ js/app.js
@@ -1,12 +1,40 @@
-var Cat = function(){
+//Preferred: store initial cats externally and read them to initialCats
+var initialCats = [
+	{
+		clickCount: 0,
+		name: "Tony",
+		imgSrc: "img/434164568_fea0ad4013_z.jpg",
+	 	imgAttribution: "https://www.flickr.com",
+	 	nicknames: ["Basketball Guy", "Genius", "Lucky Guy"]
+	},
+	{
+		clickCount: 0,
+		name: "Lucy",
+		imgSrc: "img/22252709_010df3379e_z.jpg",
+	 	imgAttribution: "https://www.flickr.com",
+	 	nicknames: ["Violin Girl", "Beauty", "Flower"]
+	},
+	{
+		clickCount: 0,
+		name: "Patrick",
+		imgSrc: "img/1413379559_412a540d29_z.jpg",
+	 	imgAttribution: "https://www.flickr.com",
+	 	nicknames: ["Handsome Boy", "Big Bird", "Smiling Boy"]
+	},
+]
+
+
+var Cat = function(data){
 	//Define models (functions are still seperated)
 	//Bind with <div data-bind="text: clickCount"></div>
+	this.clickCount = ko.observable(data.clickCount);
+
 	//Bind with h2 in html via data-bind="text: name"
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tony');
+	this.name = ko.observable(data.name);
+
 	//Bind with attr: {src: imgSrc} to set img src
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('https://www.flickr.com');
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
 
 	this.level = ko.computed(function() {
 		//Omitting "this." will raise an error.
@@ -23,16 +51,38 @@ var Cat = function(){
 
 	}, this);
 
-	this.nicknames = ko.observableArray([
-		{nickname: "Basketball Guy"},
-		{nickname: "Genius"},
-		{nickname: "Lucky Boy"}
-	]); 
+	this.nicknames = ko.observableArray();
+	for (var i=0; i<data.nicknames.length; i++) {
+		this.nicknames.push({nickname: data.nicknames[i]});
+	}
 }
 
+
+//ViewModel is what is shown on the page
 var ViewModel = function() {
-	this.currentCat = ko.observable(new Cat());
-	this.incrementCounter = function() {
+	//Bind ViewModel to "self" in case "this" changes scope
+	var self = this;
+
+	//Put all initial cats into an array
+	//Initial cats can change in external file
+	self.catList = ko.observableArray([]);
+	for (var i=0; i<initialCats.length; i++) {
+		self.catList.push(new Cat(initialCats[i]));
+	}
+
+	//The initial page will show the first cat
+	//Note: use catList()[0]. 
+	//catList[0] will output nothing
+	self.currentCat = ko.observable(self.catList()[0]);
+	
+	//Define a click listener to show clicked cat
+	self.showClickedCat = function(clickedCat) {
+		//Set currentCat to clickedCat
+		self.currentCat(clickedCat);
+	};
+
+	//Increment the click count when image is clicked
+	self.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
 	};
 }
